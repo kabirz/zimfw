25dca3eb50905c0d8b1a65e40146a87c4390420e
kabirz <jxwazxzhp@126.com>
2020-07-12 23:10:37 +0800

    init my zimfw

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # zimfw
 my simple zsh environment
+
+```bash
+chsh -s $(which zsh)
+```
+mkdir ~/.zim/
+copy zimfw.sh to ~/.zim
+
+
+## Install
+
+enter this git repo workspace then run install.sh
---
+++ install.sh
@@ -0,0 +1,11 @@
+
+mkdir ~/.zim/
+cp zimfw.sh ~/.zim
+ln -s `pwd`/zshenv ~/.zshenv
+ln -s `pwd`/zshrc ~/.zshrc
+ln -s `pwd`/zlogin ~/.zlogin
+ln -s `pwd`/zimrc ~/.zimrc
+pushd ~
+zsh ~/.zim/zimfw.zsh install
+popd
+
