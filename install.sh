f35c32fbe652dd9974239d53a5b66da52f698592
kabirz <jxwazxzhp@126.com>
2024-06-18 14:53:13 +0800

    don't link zshrc again
    Signed-off-by: kabirz <jxwazxzhp@126.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 set -e
 
-[ -e ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.old$$
 [ -d ~/.zim ]  || mkdir ~/.zim/
 cp `pwd`/zimfw.zsh ~/.zim
 [ -e ~/.zshenv ] || ln -s `pwd`/zshenv ~/.zshenv
@@ -11,5 +10,5 @@ pushd ~
 zsh ~/.zim/zimfw.zsh install
 popd
 
-ln -s `pwd`/zshrc ~/.zshrc
+echo "source `pwd`/zshrc" >> ~/.zshrc
 dircolors -p >~/.dir_colors
